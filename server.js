6f522bf811a92214be670ae6ea42e2242a9ed11b
Om Nath Kushwaha <58984004+omkushwaha14@users.noreply.github.com>
2020-04-08 21:27:05 +0545

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express =require('express')
 const path = require('path')
+const http = require('http')
 const app =express();
 app.use(express.json())
+const PORT = process.env.PORT || 5432
 app.use(express.urlencoded({extended:true}))
 app.set("view engine", "hbs")
 app.use('/',express.static(path.join(__dirname,'views')))
 app.use('/',require('./routes/users'))
 
-
-app.listen(2678,()=> console.log('server started at http://localhost:2678'))
+app.listen(PORT, () => {
+    console.log("Server listening on port");
+});
 
 
 
